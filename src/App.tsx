391bf6d8b24c0c3672f7998c81c973f969caecdf
Siri Puligadda <46283603+Siri-Puligadda@users.noreply.github.com>
2025-01-26 16:05:14 -0500

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import Itemlistmanager from "./components/Itemlistmanager";
+function App() {
+  return (
+    <>
+      <h1>React Slideshow</h1>
+      <Itemlistmanager />
+    </>
+  );
+}
+export default App;
